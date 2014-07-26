9b6860a86140961c636e39317d10d959dfa23083
Mikola Samardak <niksaak@gmail.com>
2014-07-26 15:14:34 +0300

    Justify for building on Linux

--- proj.win32/Precompiled/Precompiled.h
+++ proj.win32/Precompiled/Precompiled.h
@@ -8,7 +8,7 @@
 typedef std::string		String;
 
 #define GLFW_INCLUDE_GLU
-#include <glfw/glfw3.h>
+#include <GLFW/glfw3.h>
 
 #define __LOG(PREFIX,CONTENT)		\
 	std::cerr<< PREFIX<< CONTENT<< std::endl
